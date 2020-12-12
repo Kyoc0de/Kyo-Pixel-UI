6de7c3db11f6241d3cfb66f75e00101726fef080
Jiayuan Qiu <jiayuanqiu@Jiayuans-Mac-mini.local>
2020-12-11 21:11:20 -0500

    Button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//组件
+import Button, {ButtonSize, ButtonType} from "./components/Button/button";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>Hello</Button>
+        <Button autoFocus btnType={ButtonType.Primary} size={ButtonSize.Large} disabled={true}>Hello</Button>
+        <Button btnType={ButtonType.Link} href="www.baidu.com" size={ButtonSize.Small}>Hello</Button>
     </div>
   );
 }
